92fb868b94389d889a93accf1adf6a3e8872f80d
Ricardo Simões Costa <ricardo@Ricardos-MacBook-Pro.local>
2025-05-01 20:52:51 +0100

    feat: fix calculate yield

--- app/utils/calculate-yield.ts
+++ app/utils/calculate-yield.ts
@@ -2,17 +2,10 @@ import type { VegetableInfo } from './constants';
 
 export const calculateYield = (veggie: VegetableInfo, area: number) => {
 	// Extract just the numbers (assuming format like "3-5 kg per m²")
-	const yieldString = veggie.yieldPerPlant;
-	const match = yieldString.match(/(\d+)-(\d+)/);
 
-	if (match) {
-		const min = Number.parseInt(match[1]);
-		const max = Number.parseInt(match[2]);
-		const avgYield = (min + max) / 2;
-		//TODO: multiply time quantity
-		//return `${(avgYield * area).toFixed(1)} kg`;
-		return `${avgYield.toFixed(1)} kg`;
-	}
+	const min = Number.parseInt(veggie.yieldPerPlant.from);
+	const max = Number.parseInt(veggie.yieldPerPlant.to);
+	const avgYield = (min + max) / 2;
 
-	return veggie.yieldPerPlant;
+	return avgYield;
 };
